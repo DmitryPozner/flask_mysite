47172de6b9f8af905d82bf070ee2613ce22fd742
Dmitry Pozner <dmitry@443247.msk-kvm.ru>
2022-02-07 23:28:17 +0300

    edit ports

--- application.py
+++ application.py
@@ -15,5 +15,5 @@ def root():
 #--------Main------------------
 if __name__ == "__main__":
     application.debug = True
-    application.run(host='0.0.0.0', port=80)
+    application.run(host='0.0.0.0', port=8888)
     
