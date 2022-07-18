171e1b8323d9473e35f32348f0c74c5ea665bd6d
Dmitry Pozner <dmitry.dmitp@yandex.ru>
2022-07-18 23:21:13 +0300

    Refactoring flask. Split of html-file.

--- application.py
+++ application.py
@@ -1,13 +1,35 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 application = Flask(__name__)
-####################################
 
 @application.route("/")
 def root():
-    return render_template("index.html")
+    skills = ['Linux Servers and Windows Servers','Scripts on bash, python, powershell','CI/CD (Jenkins, Github Actions)','Docker, K8s, Ansible','Git']
+    contacts = [
+        {
+            'service' : 'GitHub',
+            'link' : 'https://github.com/DmitryPozner'
+        },
+        {
+            'service' : 'Mail', 
+            'link' : 'https://yandex.mail.ru'
+        }
+    ]
+    return render_template("index.html", title = 'Dmitry`s Web Page', skills = skills, contacts = contacts)
 
+@application.route('/login/', methods=['post', 'get'])
+def login():
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+    if username == 'root' and password == 'pass': message = "Correct username and password"
+    else:
+        message = "Wrong username or password"
+
+    return render_template('login.html', message=message)
 
 if __name__ == "__main__":
     application.debug = True
